570cde6c6a66a32347d1a8b2298bab72b0f1658f
sdob <sdob@cs.ucc.ie>
2018-12-12 18:19:06 +0000

    Receive an object

--- src/bazaar-tab/makeUpdateReserve.js
+++ src/bazaar-tab/makeUpdateReserve.js
@@ -3,8 +3,15 @@ import { RESERVE_UPDATED } from '../persistence/action-types';
 const parseAmount = amount => amount;
 
 export default function makeUpdateReserve({ store, storage }) {
-  return (amount) => {
+  return ({ amount, qualityId }) => {
     // TODO: parse the value retrieved and persist it
-    store.dispatch({ type: RESERVE_UPDATED, payload: parseAmount(amount) });
+    store.dispatch({
+      type: RESERVE_UPDATED,
+      payload: {
+        qualityId,
+        amount: parseAmount(amount)
+      },
+    });
+
   };
 }
