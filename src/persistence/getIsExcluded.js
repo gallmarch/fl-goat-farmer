50a588bb06b2bb87fc8b2a1e6bb6ac50d81b3f9e
sdob <sdob@cs.ucc.ie>
2018-12-13 14:32:20 +0000

    Cull obsolete stuff

--- src/persistence/getIsExcluded.js
+++
@@ -1,9 +0,0 @@
-/**
- * Return a function ({ characterId, id }) => bool that tells
- * us whether an item type is excluded for a given character in
- * storage.
- * @param {Object} param0
- */
-export default function getIsExcluded({ storage }) {
-  return ({ characterId, id }) => storage.getItem(`flgf_${characterId}_${id}_exclusion`) === 'X';
-}
--- src/persistence/makeItemReserveKey.js
+++
@@ -1,8 +0,0 @@
-/**
- * Create a key for this quality and this character ID to mark
- * it in localStorage as excluded from the total.
- * @param {Object} param0
- */
-export default function makeItemExclusionKey({ characterId, id }) {
-  return `flgf_${characterId}_${id}_exclusion`;
-}
