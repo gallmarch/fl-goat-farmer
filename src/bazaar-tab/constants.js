9056f2f407a34a5c72948b755bc17889e674fa45
sdob <sdob@cs.ucc.ie>
2018-12-14 12:27:03 +0000

    Give EXCHANGE_URL a protocol

--- src/bazaar-tab/constants.js
+++ src/bazaar-tab/constants.js
@@ -1,2 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export const EXCHANGE_URL = '//api.fallenlondon.com/api/exchange/availabilities?shopid=null';
+export const EXCHANGE_URL = 'https://api.fallenlondon.com/api/exchange/availabilities?shopid=null';
