04306d4a323b64ddbcf93be5ce534f78bb039f48
sdob <sdob@cs.ucc.ie>
2018-12-15 10:50:45 +0000

    Whitelist exclusion + reserve reducers

--- src/configureStore.js
+++ src/configureStore.js
@@ -6,7 +6,13 @@ import thunk from 'redux-thunk';
 import reducer from './reducer';
 
 export default function configureStore() {
-  const persistConfig = { key: 'root', storage };
+  const persistConfig = {
+    key: 'root',
+    storage,
+    // We only want to persist exclusions and reserve to storage; everything
+    // else is ephemeral
+    whitelist: ['exclusions', 'reserve'],
+  };
   const persistedReducer = persistReducer(persistConfig, reducer);
 
   const store = applyMiddleware(thunk)(createStore)(persistedReducer);
