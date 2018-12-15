ef9909665cb0923bcaa7ff4045e4743c74771fd4
sdob <sdob@cs.ucc.ie>
2018-12-15 13:27:28 +0000

    Test reserve reducer

---
+++ src/reserve/__tests__/reducer.test.js
@@ -0,0 +1,20 @@
+import { RESERVE_UPDATED } from '../action-types';
+import reducer, { INITIAL_STATE } from '../reducer';
+
+const mockCharacterId = 65536;
+jest.mock('../../auth/getCharacterId', () => () => mockCharacterId);
+
+describe('reducer', () => {
+  it('has the expected initial state', () => {
+    expect(reducer()).toEqual(INITIAL_STATE);
+  });
+
+  it('updates the correct character\'s reserve', () => {
+    const qualityId = 600;
+    const amount = 250;
+    const action = { type: RESERVE_UPDATED, payload: { amount, qualityId } };
+    expect(reducer({}, action)).toEqual({
+      [mockCharacterId]: { [qualityId]: amount },
+    });
+  });
+});
