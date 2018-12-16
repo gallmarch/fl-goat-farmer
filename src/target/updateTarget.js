066ccd32ad24746c553a4c89c204ab4d0e5c62c5
sdob <sdob@cs.ucc.ie>
2018-12-16 12:10:56 +0000

    Test updateTarget

--- src/target/updateTarget.js
+++ src/target/updateTarget.js
@@ -3,11 +3,15 @@ import getCharacterId from '../auth/getCharacterId';
 
 import { TARGET_UPDATED } from './action-types';
 
-export default function updateTarget() {
+export default updateTarget(window);
+
+export const PROMPT_TEXT = 'Enter your new target, in Echoes';
+
+export function updateTarget(window) {
   return (dispatch, getState) => {
     const characterId = getCharacterId();
     const target = window.prompt(
-      'Enter your new target, in Echoes',
+      PROMPT_TEXT,
       getState().target[characterId] || 0,
     );
     const action = { type: TARGET_UPDATED, payload: target };
