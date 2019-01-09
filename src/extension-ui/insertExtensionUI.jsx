dc0dacdd0640782daef72ea3a9b24a0d6170e5f9
sdob <sdob@cs.ucc.ie>
2019-01-09 18:04:05 +0000

    Don't reset target if user cancels out of update prompt

--- src/extension-ui/insertExtensionUI.jsx
+++ src/extension-ui/insertExtensionUI.jsx
@@ -1,8 +1,12 @@
+/* eslint-disable no-alert */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import ExtensionUI from '../extension-ui/ExtensionUI';
+import makeUpdateTarget from '../target/makeUpdateTarget';
+
+export const PROMPT_TEXT = 'Enter your new target, in Echoes';
 
 /**
  * Insert the extension's UI into the DOM, with the store as its provider
@@ -11,11 +15,29 @@ import ExtensionUI from '../extension-ui/ExtensionUI';
 export default function insertExtensionUI({ store }) {
   const parent = document.querySelector('.exchange__title').parentNode;
   const container = document.createElement('div');
+
+  const updateTarget = makeUpdateTarget({ store });
+
+  // Handle clicks to the 'update' button
+  const onClick = () => {
+    // Get the current character ID
+    const { auth: { characterId } } = store.getState();
+    const target = window.prompt(
+      PROMPT_TEXT,
+      store.getState().target[characterId] || 0,
+    );
+    // If the user has cancelled, then return
+    if (target === null) {
+      return;
+    }
+    store.dispatch(updateTarget(parseFloat(target)));
+  };
+
   container.id = 'flgf-root';
   parent.insertBefore(container, parent.querySelector('hr'));
   ReactDOM.render(
     <Provider store={store}>
-      <ExtensionUI />
+      <ExtensionUI onClick={onClick} />
     </Provider>,
     container,
   );
