c6c204ead930092c783ebc356cc8e0b6b0f2705c
sdob <sdob@cs.ucc.ie>
2019-01-24 11:22:24 +0000

    Check for things that sell for Primordial Shrieks

--- src/extension-ui/selectors/constants.js
+++ src/extension-ui/selectors/constants.js
@@ -1,9 +1,11 @@
 export const AN_IDENTITY_UNCOVERED = 657;
 export const CRYPTIC_CLUE = 389;
+export const PRIMORDIAL_SHRIEK = 388;
 export const TOUCHING_LOVE_STORY = 945;
 
 export const SELL_PRICES = {
   [AN_IDENTITY_UNCOVERED]: 250,
   [CRYPTIC_CLUE]: 2,
+  [PRIMORDIAL_SHRIEK]: 2,
   [TOUCHING_LOVE_STORY]: 250,
 };
